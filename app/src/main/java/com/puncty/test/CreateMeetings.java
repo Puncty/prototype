bc4f960cf43aebd1fb8c383b55e8320d9e71df8c
Datengeist <janikfo04@gmail.com>
2023-01-31 15:30:27 +0100

    add library v0.1.3 to java folder

--- app/src/main/java/com/puncty/test/CreateMeetings.java
+++ app/src/main/java/com/puncty/test/CreateMeetings.java
@@ -1,14 +1,43 @@
 package com.puncty.test;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.puncty.lib.Meetup;
+import com.puncty.lib.MeetupCollection;
+import com.puncty.lib.Session;
+import com.puncty.lib.exceptions.BrokenResponse;
+import com.puncty.lib.networking.Requester;
+
+import java.util.Date;
 
 public class CreateMeetings extends AppCompatActivity {
 
+    public Session s;
+    public MeetupCollection mc;
+
+    public EditText editTextDate;
+    public EditText editTextTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meetings);
+        editTextDate = findViewById(R.id.editTextDate);
+        editTextTime = findViewById(R.id.editTextTime);
+
+        Requester r = new Requester("https://api.puncty.com");
+        Session s = null;
+        try {
+            s = Session.login(r, "test@test.com", "test");
+        } catch (BrokenResponse e) {
+            System.out.println(e);
+        }
+
+        mc = new MeetupCollection(s);
+    }
+
+    private void createMeetingButton(){
+        Date meeting = new Date();
     }
 }
